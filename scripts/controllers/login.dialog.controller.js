7fcc92f03fe5cf970a21108f9c6099252082cd2a
Mauricio Lara Soberanis <mauricio9308@gmail.com>
2016-06-20 18:44:29 -0500

    Adding the login dialog

---
+++ scripts/controllers/login.dialog.controller.js
@@ -0,0 +1,16 @@
+/**
+ * Created by mauriciolara on 6/20/16.
+ */
+(function(){
+    'use strict';
+
+    angular.module('alertSystem').controller('LoginDialogController', LoginDialogController);
+
+    /**
+     * Controller dialog for the login sequence
+     * */
+    function LoginDialogController(){
+        var vm = this;
+    }
+
+}());
