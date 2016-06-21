b9116b666bb20dbf29a6895a323662f7445ba705
Rob McDiarmid <robianmcd@gmail.com>
2016-06-20 22:39:08 -0400

    added angularfire

--- scripts/app.js
+++ scripts/app.js
@@ -1,35 +1,8 @@
 (function() {
   'use strict';
 
-  var initialWeatherForecast = {
-    key: 'newyork',
-    label: 'New York, NY',
-    currently: {
-      time: 1453489481,
-      summary: 'Clear',
-      icon: 'partly-cloudy-day',
-      temperature: 52.74,
-      apparentTemperature: 74.34,
-      precipProbability: 0.20,
-      humidity: 0.77,
-      windBearing: 125,
-      windSpeed: 1.52
-    },
-    daily: {
-      data: [
-        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
-      ]
-    }
-  };
-
   //Declaration of the application module
-  var angularApp = angular.module('alertSystem', ['ngMaterial', 'ngStorage']);
+  var angularApp = angular.module('alertSystem', ['ngMaterial', 'ngStorage', 'firebase']);
 
   // Setting application configuration
   angular
@@ -57,7 +30,7 @@
     selectedCities: [],
     spinner: document.querySelector('.loader'),
     cardTemplate: document.querySelector('.cardTemplate'),
-    container: document.querySelector('.main'),
+    container: document.querySelector('.main')
   };
 
 
