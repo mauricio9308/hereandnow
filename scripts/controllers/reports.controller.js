ac817a2db5c2f5a3ea3de1f55d94497a7db34742
Ivancho <ivancho@gmail.com>
2016-06-21 03:16:51 -0500

    inflating the reports listview

--- scripts/controllers/reports.controller.js
+++ scripts/controllers/reports.controller.js
@@ -3,40 +3,13 @@
  */
 (function(){
 
-    angular.module('alertSystem').controller('ReportsController', ReportsController);
-
-    ReportsController.$inject = ['LevelColorsService'];
-
-    function ReportsController(LevelColorsService) {
+    var app = angular.module('alertSystem')
+    app.controller('ReportsController', ReportsController);
+    ReportsController.$inject = ['LevelColorsService', '$firebaseArray'];
+   
+    function ReportsController(LevelColorsService, $firebaseArray) {
         var vm = this;
-
-        function load(){
-            /* mock objects for the display of the alerts */
-            vm.reports = [
-                {
-                    authorUid: 123,
-                    date: "2016-06-10",
-                    description: "An event ocurred",
-                    isAnonymous : false,
-                    level: 1,
-                    longitude: 20,
-                    latitude: 20
-                }
-            ];
-
-            /* we add the color parsing to each of the objects */
-            for( var i = 0, total = vm.reports.length; i < total; i ++ ){
-                //Parsing the color of the alert level
-                var reportLevel = vm.reports[i].level;
-                vm.reports[i].color = LevelColorsService.getLevelColor( reportLevel );
-                vm.reports[i].levelName = LevelColorsService.getLevelName( reportLevel );
-            }
-
-            console.log( vm.reports );
-            console.log( vm.reports.length );
-        }
-
-        load();
+        var ref = firebase.database().ref().child("reports");
+        vm.reports = new ColorFactory(ref);
     }
-
-}(firebase));
+}(firebase))
