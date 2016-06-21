c67a365bde9f629f92ac8154d72abca0cf3a3a3c
Mauricio Lara Soberanis <mauricio9308@gmail.com>
2016-06-20 19:08:12 -0500

    Adding Google Plus references

--- scripts/controllers/login.dialog.controller.js
+++ scripts/controllers/login.dialog.controller.js
@@ -6,11 +6,21 @@
 
     angular.module('alertSystem').controller('LoginDialogController', LoginDialogController);
 
+    //Injection of the dependencies
+    LoginDialogController.$inject = ['GoogleLoginService'];
+
     /**
      * Controller dialog for the login sequence
      * */
-    function LoginDialogController(){
+    function LoginDialogController(GoogleLoginService){
         var vm = this;
+
+        /**
+         * Triggers the login process for the application
+         * */
+        vm.loginWithGooglePlus = function(){
+            GoogleLoginService.googleLogin();
+        };
     }
 
 }());
