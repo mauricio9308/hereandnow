ba7925c5af348e353dcd4c36663bb84a88fab22a
Mauricio Lara Soberanis <mauricio9308@gmail.com>
2016-06-21 06:39:21 -0500

    Adding new steps for the danger and caution reports

--- scripts/controllers/add.report.dialog.controller.js
+++ scripts/controllers/add.report.dialog.controller.js
@@ -8,12 +8,12 @@
     angular.module('alertSystem').controller('AddReportDialogController', AddReportDialogController);
 
     /* injecting the dependencies */
-    AddReportDialogController.$inject = ['ReportsService', '$mdDialog', '$scope', '$mdMedia'];
+    AddReportDialogController.$inject = ['$mdDialog', '$scope', '$mdMedia'];
 
     /**
      * Controller for adding a given report to the actual location
      * */
-    function AddReportDialogController(ReportsService, $mdDialog, $scope, $mdMedia) {
+    function AddReportDialogController($mdDialog, $scope, $mdMedia) {
         var vm = this;
 
         /**
@@ -22,13 +22,21 @@
         vm.createReport = function (ev, reportKind) {
             /* create a dialog for the creation of a report */
             var useFullScreen = ($mdMedia('sm') || $mdMedia('xs') || $mdMedia('md'));
+
+            /* setting the next step in the builder */
+            var controller = ( reportKind <= 2 ? 'BuildReportDialogController as vm' :
+                'SelectEventReportController as vm' );
+            var templateUrl = ( reportKind <= 2 ? 'dialogs/build.report.dialog.html'
+                : 'dialogs/select.event.report.dialog.html');
+
             $mdDialog.show({
-                controller: 'BuildReportDialogController as vm',
-                templateUrl: 'dialogs/build.report.dialog.html',
+                controller: controller,
+                templateUrl: templateUrl,
                 parent: angular.element(document.body),
                 targetEvent: ev,
                 locals: {
-                    level: reportKind
+                    level: reportKind,
+                    event: undefined
                 },
                 clickOutsideToClose: false,
                 fullscreen: useFullScreen
--- scripts/controllers/build.report.dialog.controller.js
+++ scripts/controllers/build.report.dialog.controller.js
@@ -8,13 +8,13 @@
 
     /* injecting the report message */
     BuildReportDialogController.$inject = ['ReportsService', 'level', 'LevelColorsService',
-        '$mdDialog', '$localStorage', 'ToastService', '$rootScope'];
+        '$mdDialog', '$localStorage', 'ToastService', '$rootScope', 'event'];
 
     /**
      * Controller in charge of the report creation
      * */
     function BuildReportDialogController( ReportsService, level, LevelColorsService,
-                                          $mdDialog, $localStorage, ToastService, $rootScope){
+                                          $mdDialog, $localStorage, ToastService, $rootScope, event){
         var vm = this;
 
         //Loading flag
@@ -25,6 +25,8 @@
             //We make only level 2 or less alert kinds public by default (Message and lost)
             isAnonymous : ( level <= 2 ? false : true )
         };
+
+        console.log('passed event: ' + event);
 
         /**
          * Getting the reference of the given report level
